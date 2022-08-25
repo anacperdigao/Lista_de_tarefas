5a7be2f573308e8a051bb29a3706b09770d40592
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-08-25 17:55:30 -0300

    Melhorando CSS e add troca de theme no js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 /*Users should be able to:
 
 - View the optimal layout for the app depending on their device's screen size
-- See hover states for all interactive elements on the page
 - Add new todos to the list
 - Mark todos as complete
 - Delete todos from the list
 - Filter by all/active/complete todos
 - Clear all completed todos
-- Toggle light and dark mode
 - **Bonus**: Drag and drop to reorder items on the list
-
 */
+
+
+//Trocando dark e white mode
+
+const html = document.querySelector("html")
+const iconeTema = document.querySelector(".icone__theme")
+
+iconeTema.addEventListener("click", trocaTema)
+
+function trocaTema(){
+    html.classList.toggle("white__mode")
+}
+
