366e73aad65713edf29343d77892c55e7db60258
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-08-25 10:56:53 -0300

    Criando inicio do HTML e CSS

---
+++ script.js
@@ -0,0 +1,13 @@
+/*Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Add new todos to the list
+- Mark todos as complete
+- Delete todos from the list
+- Filter by all/active/complete todos
+- Clear all completed todos
+- Toggle light and dark mode
+- **Bonus**: Drag and drop to reorder items on the list
+
+*/
