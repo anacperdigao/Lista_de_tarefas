8eef1347d0494d3ee79dfad2c7852362c85949ce
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-08-27 12:34:37 -0300

    Ajustando bug da troca do icone em troca-tema.js

--- js/troca-tema.js
+++ js/troca-tema.js
@@ -2,26 +2,34 @@
 
 - View the optimal layout for the app depending on their device's screen size
 - Mark todos as complete
-- Delete todos from the list
 - Filter by all/active/complete todos
 - Clear all completed todos
 - **Bonus**: Drag and drop to reorder items on the list
 */
 
 
-//Trocando dark e white mode
-
 const html = document.querySelector("html")
 const iconeTema = document.querySelector(".icone__theme")
-const imagemBackground = document.querySelector(".cabecalho")
+var num = 2
+//const imagemBackground = document.querySelector(".cabecalho")
 
 iconeTema.addEventListener("click", trocaTema)
 
 function trocaTema(){
-    if(iconeTema.src = "/assets/images/icon-sun.svg"){
-        iconeTema.src = "/assets/images/icon-moon.svg"
-        html.classList.toggle("white__mode")
 
+    html.classList.toggle("white__mode")
+
+    setTimeout(function () {
+    
+    if (num == 1){
+        iconeTema.src = "/assets/images/icon-sun.svg";
+    
+    } else if (num == 2){
+        iconeTema.src = "/assets/images/icon-moon.svg";
+    
     }
-}
 
+    //garante que num fique alternando entre 1 e 2    
+    num = (num % 2) + 1;
+    });
+}
