7cb6899b8485efa15708a49a1112e57b2f33fdb2
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-08-26 21:42:43 -0300

    Add funcionalidade exclui-tarefa no JS

--- js/troca-tema.js
+++ js/troca-tema.js
@@ -1,7 +1,6 @@
 /*Users should be able to:
 
 - View the optimal layout for the app depending on their device's screen size
-- Add new todos to the list
 - Mark todos as complete
 - Delete todos from the list
 - Filter by all/active/complete todos
